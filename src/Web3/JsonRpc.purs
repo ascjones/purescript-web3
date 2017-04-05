211c88af25b70b31acbfcf538e41f2289003a701
Andrew Jones <ascjones@gmail.com>
2017-04-05 08:00:53 +0100

    JsonRpc Encode/Decode Json compiles

--- src/Web3/JsonRpc.purs
+++ src/Web3/JsonRpc.purs
@@ -3,13 +3,20 @@ module Web3.JsonRpc
   , JsonRpcResponse
   ) where
 
+import Prelude
 import Control.Monad.Aff (Aff)
 import Control.Monad.Aff.AVar (AVAR)
 import Data.Argonaut.Core (Json, jsonEmptyObject)
+import Data.Argonaut.Decode (class DecodeJson, decodeJson, (.?))
 import Data.Argonaut.Encode (class EncodeJson, encodeJson, (:=), (~>))
-import Data.Either (Either)
-import Data.Maybe (Maybe)
+import Data.Either (Either(..))
+import Data.Maybe (Maybe(..))
 
+-- todo: see whether we can use generic deriving for encode/decode
+
+--
+-- Request
+--
 newtype JsonRpcRequest a =
   JsonRpcRequest
   { jsonrpc :: String
@@ -26,41 +33,57 @@ instance encodeJsonRpcRequesqt :: EncodeJson a => EncodeJson (JsonRpcRequest a) 
     ~> "params" := req.params
     ~> jsonEmptyObject
 
+--
+-- Response
+--
 newtype JsonRpcResponse a =
   JsonRpcResponse (Either JsonRpcFailure (JsonRpcSuccess a))
 
--- instance decodeJsonRpcResponse :: DecodeJson JsonRpcResponse where
---   decodeJson = gDecodeJson
-
-data JsonRpcSuccess a =
+newtype JsonRpcSuccess a =
   JsonRpcSuccess
   { jsonrpc :: String
   , id :: Int
   , result :: a
   }
 
-data JsonRpcFailure =
+newtype JsonRpcFailure =
   JsonRpcFailure
   { jsonrpc :: String
   , id :: Maybe Int
   , error :: String
   }
 
+instance decodeJsonRpcResponse :: DecodeJson a => DecodeJson (JsonRpcResponse a) where
+  decodeJson json = do
+    pure $ JsonRpcResponse $ Left $ JsonRpcFailure { jsonrpc : "jsonrpc", id : Nothing, error : "error" }
+
+instance decodeJsonRpcFailure :: DecodeJson a => DecodeJson JsonRpcFailure where
+  decodeJson json = do
+    obj <- decodeJson json
+    jsonrpc <- obj .? "jsonrpc"
+    id <- obj .? "id"
+    error <- obj .? "error"
+    pure $ JsonRpcFailure { jsonrpc, id, error }
+
+instance decodeJsonRpcSuccess :: DecodeJson a => DecodeJson (JsonRpcSuccess a) where
+  decodeJson json = do
+    obj <- decodeJson json
+    jsonrpc <- obj .? "jsonrpc"
+    id <- obj .? "id"
+    result <- obj .? "result"
+    pure $ JsonRpcSuccess { jsonrpc, id, result }
+
 type SendJsonRpcRequest e = Json -> Aff (avar :: AVAR | e) Json
 
--- sendRequest
---   :: forall e a b
---    . (SendJsonRpcRequest e)
---   -> (JsonRpcRequest a)
---   -> Aff (avar :: AVAR | e) (JsonRpcResponse b)
--- sendRequest send req = do
---   responseJson <- send reqJson
---   let response = decodeJson responseJson
---   pure response
---
---   where
---
---   reqJson = encodeJson req
-
-encode :: forall a. EncodeJson a => JsonRpcRequest a -> Json
-encode = encodeJson
+sendRequest
+  :: forall e a b
+   . (EncodeJson a, DecodeJson b)
+  => (SendJsonRpcRequest e)
+  -> (JsonRpcRequest a)
+  -> Aff (avar :: AVAR | e) (JsonRpcResponse b)
+sendRequest send req = do
+  responseJson <- send $ encodeJson req
+  let response = decodeJson responseJson
+  case response of
+    Left _ -> pure $ JsonRpcResponse $ Left $ JsonRpcFailure { jsonrpc : "jsonrpc", id : Nothing, error : "error" }
+    Right res -> pure res
